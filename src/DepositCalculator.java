71c572bc842ba41e771375cdeb53439f2108e3b1
Viktor Shilkov <bourbon.tommy@yandex.ru>
2023-11-16 20:14:03 +0300

    update

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class DepositCalculator {
     public static void main(String[] args) {
-        DepositCalculator depositCalculator = new DepositCalculator();
-        depositCalculator.chooseDeposit();
+        new DepositCalculator().chooseDeposit();
     }
 
     double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
